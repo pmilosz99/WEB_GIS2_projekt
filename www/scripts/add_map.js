b1ac2496ec1a4a7103eedafbc834b4c329000d68
pmilosz99 <piter31@poczta.onet.pl>
2022-01-24 19:21:04 +0100

    improve map and done area text and footer

--- www/scripts/add_map.js
+++ www/scripts/add_map.js
@@ -2,86 +2,135 @@ require([
     "esri/Map",
     "esri/views/MapView",
     "esri/layers/FeatureLayer",
-    "esri/Graphic",
-    "esri/layers/GraphicsLayer",
     "esri/widgets/BasemapGallery",
+    "esri/widgets/LayerList",
+    "esri/widgets/Legend",
+    "esri/widgets/Measurement",
+    "esri/widgets/Search",
     "esri/widgets/Expand",
-], (Map, MapView, FeatureLayer, Graphic, GraphicsLayer, BasemapGallery, Expand) => {
+], (Map, MapView, FeatureLayer, BasemapGallery, LayerList, Legend, Measurement, Search, Expand) => {
 
+    // add map
 
     const map1 = new Map({
-        basemap: "topo-vector"
+        basemap: "streets-vector"
     });
 
     const view = new MapView({
         map: map1,
         container: "mapDiv",
-        center:[21.006, 52.234],
-        zoom:15
+        center:[21.009512, 52.235382],
+        zoom:13
     });
+
+    // grafic links (zoom to district)
 
     document.getElementById("zoliborz").addEventListener("click", function() {
         view.goTo({
-            center: [20.957, 52.233],
+            center: [20.975249, 52.265770],
             zoom: 15
         });
     });
 
-    // Warstwy
-    const gl = new GraphicsLayer();
+    document.getElementById("srodmiescie").addEventListener("click", function() {
+        view.goTo({
+            center: [21.009512, 52.235382],
+            zoom: 15
+        });
+    });
+
+    document.getElementById("mokotow").addEventListener("click", function() {
+        view.goTo({
+            center: [21.008178, 52.195381],
+            zoom: 15
+        });
+    });
+
+    document.getElementById("wola").addEventListener("click", function() {
+        view.goTo({
+            center: [20.970851, 52.240052],
+            zoom: 15
+        });
+    });
+
+    // Layers
 
     const fl = new FeatureLayer({
         url: "https://services.arcgis.com/hVsb2InA4op6ydG0/arcgis/rest/services/zabytki_wawa/FeatureServer"
     });
 
     map1.add(fl);
-    map1.add(gl);
 
-    // Grafiki
+    // Popups
 
-    const geom = {
-        type: "polyline",
-        paths: [[-96.06326,33.759],[-97.06298,32.755]]
-    };
+    const template = {
+        title: "Informacje o obiekcie",
+        content: [
+            {
+                type: "fields",
+                fieldInfos: [
+                    {
+                        fieldName: "name",
+                        label: "Nazwa zabytku"
 
-    const sym = {
-        type:"simple-line",
-        color: "blue",
-        width: 2,
-        style: "dash"
-    };
-
-    const attr = {
-        country: "Polska",
-        code: "POL"
-    };
-
-    const popTmpl = {
-        title: "Obiekt Web-GIS",
-        content: "Zaznaczony obiekt pochodzi z kraju {country}"
+                    }
+                ]
+            }
+        ]
     }
 
-    const graph = new Graphic({
-        geometry: geom,
-        symbol: sym,
-        attributes: attr,
-        popupTemplate: popTmpl
-    });
+    fl.popupTemplate = template;
 
-    gl.add(graph);
-
-    // Widgety
+    // Widgets
 
     const bmWg = new BasemapGallery({
         view: view
     });
+
+    const lyLi = new LayerList({
+        view: view
+    });
+
+    const legend = new Legend({
+        view: view
+    });
+
+    const meas = new Measurement({
+        view: view
+    });
+
+    const search = new Search({
+        view: view
+    });
+
+    // Expanding widgets
 
     const expandWg = new Expand({
         view: view,
         content: bmWg
     });
 
-    view.ui.add(expandWg, {
+    const expandLyLi = new Expand({
+        view: view,
+        content: lyLi
+    });
+
+    const expandLegend = new Expand({
+        view: view,
+        content: legend
+    });
+
+    const expandMeas = new Expand({
+        view: view,
+        content: meas
+    });
+
+    const expandSearch = new Expand({
+        view: view,
+        content: search
+    });
+
+    view.ui.add([expandWg, expandLyLi, expandLegend, expandMeas, expandSearch], {
         position: "top-right"
     });
 });
